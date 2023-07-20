e646c1b0ae429c6946d20dcaf135f445f5b6226c
yuda25 <yuda7246@gmail.com>
2023-07-20 14:30:58 +0700

    add sidebar and blade teplating

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,182 +1,142 @@
-<!doctype html>
-<html>
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-    <title>Dashboard</title>
-    <style>
-        body {
-            font-family: "Poppins";
-        }
-    </style>
-</head>
-    <body>
-    <div class="container mx-auto mt-[75px]">
+@extends('layouts.sidebar')
+@section('title', 'Dashboard')
+@section('judul1', 'Home')
+@section('judul2', 'Dashboard')
 
-        <div>
-            <nav class="justify-between py-3 text-gray-700 sm:flex" aria-label="Breadcrumb">
-                <ol class="inline-flex items-center mb-3 space-x-1 md:space-x-3 sm:mb-0">
-                    <li>
-                        <div>
-                            <svg class="w-[20px] h-[20px] text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
-                                <path d="m19.707 9.293-2-2-7-7a1 1 0 0 0-1.414 0l-7 7-2 2a1 1 0 0 0 1.414 1.414L2 10.414V18a2 2 0 0 0 2 2h3a1 1 0 0 0 1-1v-4a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v4a1 1 0 0 0 1 1h3a2 2 0 0 0 2-2v-7.586l.293.293a1 1 0 0 0 1.414-1.414Z"/>
-                            </svg>
-                        </div>
-                    </li>
-                    <li>
-                        <div class="flex items-center">
-                            <a href="#" class="ml-1 text-sm font-medium text-gray-700 hover:text-blue-600 md:ml-2 dark:text-gray-400 dark:hover:text-white">Home</a>
-                        </div>
-                    </li>
-                    <li aria-current="page">
-                        <div class="flex items-center">
-                            <svg class="w-3 h-3 mx-1 text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
-                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 9 4-4-4-4"/>
-                            </svg>
-                            <a href="#" class="ml-1 text-sm font-medium text-gray-700 hover:text-blue-600 md:ml-2 dark:text-gray-400 dark:hover:text-white">Dashboard</a>
-                        </div>
-                    </li>
-                </ol>
-            </nav>
-        </div>
+@section('body')
+    <header class="flex justify-between">
+        <p class="text-2xl">All Users</p>
+        <form>
+            <div class="relative">
+                <input type="search" class="block w-[392px] p-2 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500" placeholder="Search" required>
+            </div>
+        </form>
+    </header>
 
-        <header class="flex justify-between">
-                <p class="text-2xl">All Users</p>
-            <form>
-                <div class="relative">
-                    <input type="search" class="block w-[392px] p-2 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500" placeholder="Search" required>
-                </div>
-            </form>
-        </header>
+    <div class="relative overflow-x-auto shadow-md sm:rounded-lg mt-[35px]">
+        <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+            <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+            <tr>
+                <th scope="col" class="px-6 py-3">
+                    No
+                </th>
+                <th scope="col" class="px-6 py-3">
+                    Name
+                </th>
+                <th scope="col" class="px-6 py-3">
+                    Telephone
+                </th>
+                <th scope="col" class="px-6 py-3">
+                    NIK
+                </th>
+                <th scope="col" class="px-6 py-3">
+                    Alamat
+                </th>
+                <th scope="col" class="px-6 py-3">
+                    RT
+                </th>
+                <th scope="col" class="px-6 py-3">
+                    RW
+                </th>
+                <th scope="col" class="px-6 py-3">
+                    Kelurahan
+                </th>
+                <th scope="col" class="px-6 py-3">
+                    Kecamatan
+                </th>
+                <th scope="col" class="px-6 py-3">
+                    Lokasi TPS
+                </th>
+                <th scope="col" class="px-6 py-3">
+                    Keterangan
+                </th>
+                <th scope="col" class="px-6 py-3">
+                    Created by
+                </th>
+                <th scope="col" class="px-6 py-3">
+                    Action
+                </th>
+            </tr>
+            </thead>
+            <tbody>
+            <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                    1
+                </th>
+                <td class="px-6 py-4">
+                    Pragos
+                </td>
+                <td class="px-6 py-4">
+                    08451785963
+                </td>
+                <td class="px-6 py-4">
+                    12345678912345
+                </td>
+                <td class="px-6 py-4">
+                    Purgatory
+                </td>
+                <td class="px-6 py-4">
+                    002
+                </td>
+                <td class="px-6 py-4">
+                    001
+                </td>
+                <td class="px-6 py-4">
+                    Brazilia
+                </td>
+                <td class="px-6 py-4">
+                    Filed
+                </td>
+                <td class="px-6 py-4">
+                    Central
+                </td>
+                <td class="px-6 py-4">
+                    Warga
+                </td>
+                <td class="px-6 py-4">
+                    Kelly
+                </td>
+                <td class="px-6 py-4">
 
+                    <button data-modal-target="popup-modal" data-modal-toggle="popup-modal" type="button">
+                        <img src="assets/dashboard/detail_pemilih.png" alt="detail_pemilih">
+                    </button>
 
-        <div class="relative overflow-x-auto shadow-md sm:rounded-lg mt-[35px]">
-            <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
-                <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
-                    <tr>
-                        <th scope="col" class="px-6 py-3">
-                            No
-                        </th>
-                        <th scope="col" class="px-6 py-3">
-                            Name
-                        </th>
-                        <th scope="col" class="px-6 py-3">
-                            Telephone
-                        </th>
-                        <th scope="col" class="px-6 py-3">
-                            NIK
-                        </th>
-                        <th scope="col" class="px-6 py-3">
-                            Alamat
-                        </th>
-                        <th scope="col" class="px-6 py-3">
-                            RT
-                        </th>
-                        <th scope="col" class="px-6 py-3">
-                            RW
-                        </th>
-                        <th scope="col" class="px-6 py-3">
-                            Kelurahan
-                        </th>
-                        <th scope="col" class="px-6 py-3">
-                            Kecamatan
-                        </th>
-                        <th scope="col" class="px-6 py-3">
-                            Lokasi TPS
-                        </th>
-                        <th scope="col" class="px-6 py-3">
-                            Keterangan
-                        </th>
-                        <th scope="col" class="px-6 py-3">
-                            Created by
-                        </th>
-                        <th scope="col" class="px-6 py-3">
-                            Action
-                        </th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
-                        <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                            1
-                        </th>
-                        <td class="px-6 py-4">
-                            Pragos
-                        </td>
-                        <td class="px-6 py-4">
-                            08451785963
-                        </td>
-                        <td class="px-6 py-4">
-                            12345678912345
-                        </td>
-                        <td class="px-6 py-4">
-                            Purgatory
-                        </td>
-                        <td class="px-6 py-4">
-                            002
-                        </td>
-                        <td class="px-6 py-4">
-                            001
-                        </td>
-                        <td class="px-6 py-4">
-                            Brazilia
-                        </td>
-                        <td class="px-6 py-4">
-                            Filed
-                        </td>
-                        <td class="px-6 py-4">
-                            Central
-                        </td>
-                        <td class="px-6 py-4">
-                            Warga
-                        </td>
-                        <td class="px-6 py-4">
-                            Kelly
-                        </td>
-                        <td class="px-6 py-4">
-
-                            <button data-modal-target="popup-modal" data-modal-toggle="popup-modal" type="button">
-                                <img src="assets/dashboard/detail_pemilih.png" alt="detail_pemilih">
-                            </button>
-
-                            <div id="popup-modal" tabindex="-1" class="fixed top-0 left-0 right-0 z-50 hidden p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-[calc(100%-1rem)] max-h-full">
-                                <div class="relative w-full max-w-md max-h-full">
-                                    <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
-                                        <button type="button" class="absolute top-3 right-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ml-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-hide="popup-modal">
-                                            <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
-                                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
-                                            </svg>
-                                            <span class="sr-only">Close modal</span>
+                    <div id="popup-modal" tabindex="-1" class="fixed top-0 left-0 right-0 z-50 hidden p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-[calc(100%-1rem)] max-h-full">
+                        <div class="relative w-full max-w-md max-h-full">
+                            <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
+                                <button type="button" class="absolute top-3 right-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ml-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-hide="popup-modal">
+                                    <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
+                                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
+                                    </svg>
+                                    <span class="sr-only">Close modal</span>
+                                </button>
+                                <div class="p-10 text-center">
+                                    <div class="flex justify-between">
+                                        <button data-modal-hide="popup-modal" type="button" class="w-full text-white bg-red-600 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm inline-flex items-center px-5 py-2.5 text-center mr-2">
+                                            Reject
                                         </button>
-                                        <div class="p-10 text-center">
-                                            <div class="flex justify-between">
-                                                <button data-modal-hide="popup-modal" type="button" class="w-full text-white bg-red-600 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm inline-flex items-center px-5 py-2.5 text-center mr-2">
-                                                    Reject
-                                                </button>
-                                                <button data-modal-hide="popup-modal" type="button" class="w-full text-white bg-green-600 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 dark:focus:ring-green-800 font-medium rounded-lg text-sm inline-flex items-center px-5 py-2.5 text-center mr-2">
-                                                    Approve
-                                                </button>
-                                            </div>
-                                            <button data-modal-hide="popup-modal" type="button" class="w-full mt-[21px] text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-gray-200 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600">cancel</button>
-                                        </div>
+                                        <button data-modal-hide="popup-modal" type="button" class="w-full text-white bg-green-600 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 dark:focus:ring-green-800 font-medium rounded-lg text-sm inline-flex items-center px-5 py-2.5 text-center mr-2">
+                                            Approve
+                                        </button>
                                     </div>
+                                    <button data-modal-hide="popup-modal" type="button" class="w-full mt-[21px] text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-gray-200 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600">cancel</button>
                                 </div>
                             </div>
+                        </div>
+                    </div>
 
-                        </td>
-                    </tr>
-                </tbody>
-            </table>
-        </div>
+                </td>
+            </tr>
+            </tbody>
+        </table>
+    </div>
 
-
-
-        <div class="flex mt-[32px] justify-between">
-            <div>
-                 <span class="text-sm text-gray-700 dark:text-gray-400">
-                      Showing <span class="font-semibold text-gray-900 dark:text-white">1</span> to <span class="font-semibold text-gray-900 dark:text-white">10</span> of <span class="font-semibold text-gray-900 dark:text-white">100</span>
-                  </span>
+    <div class="flex mt-[32px] justify-between">
+        <div>
+            <span class="text-sm text-gray-700 dark:text-gray-400">
+                Showing <span class="font-semibold text-gray-900 dark:text-white">1</span> to <span class="font-semibold text-gray-900 dark:text-white">10</span> of <span class="font-semibold text-gray-900 dark:text-white">100</span>
+            </span>
             </div>
             <div class="flex">
                 <a href="#" class="flex items-center justify-center px-3 h-8 mr-3 text-sm font-medium text-white bg-blue-600 border border-blue-300 rounded-lg hover:bg-blue-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">
@@ -193,8 +153,4 @@
                 </a>
             </div>
         </div>
-
-    </div>
-    <script src="{{ asset('js/app.js') }}"></script>
-    </body>
-</html>
+@endsection
---
+++ resources/views/layouts/sidebar.blade.php
@@ -0,0 +1,97 @@
+<!doctype html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <title>@yield('title')</title>
+    <style>
+        body {
+            font-family: "Poppins";
+        }
+    </style>
+</head>
+<body>
+    <aside id="logo-sidebar" class="fixed top-0 left-0 z-40 w-64 h-screen pt-10 transition-transform -translate-x-full bg-white border-r border-gray-200 sm:translate-x-0 dark:bg-gray-800 dark:border-gray-700" aria-label="Sidebar">
+        <div class="h-full px-3 pb-4 overflow-y-auto bg-white dark:bg-gray-800">
+            <div class="flex justify-center md:justify-center lg:justify-center my-6">
+                <img src="assets/LOGO_KPU_RI.png" alt="logo">
+            </div>
+            <ul class="space-y-2 font-medium">
+                <li>
+                    <a href="#" class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
+                        <svg class="w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 21">
+                            <path d="M16.975 11H10V4.025a1 1 0 0 0-1.066-.998 8.5 8.5 0 1 0 9.039 9.039.999.999 0 0 0-1-1.066h.002Z"/>
+                            <path d="M12.5 0c-.157 0-.311.01-.565.027A1 1 0 0 0 11 1.02V10h8.975a1 1 0 0 0 1-.935c.013-.188.028-.374.028-.565A8.51 8.51 0 0 0 12.5 0Z"/>
+                        </svg>
+                        <span class="ml-3">Dashboard</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="#" class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
+                        <svg class="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 18">
+                            <path d="M14 2a3.963 3.963 0 0 0-1.4.267 6.439 6.439 0 0 1-1.331 6.638A4 4 0 1 0 14 2Zm1 9h-1.264A6.957 6.957 0 0 1 15 15v2a2.97 2.97 0 0 1-.184 1H19a1 1 0 0 0 1-1v-1a5.006 5.006 0 0 0-5-5ZM6.5 9a4.5 4.5 0 1 0 0-9 4.5 4.5 0 0 0 0 9ZM8 10H5a5.006 5.006 0 0 0-5 5v2a1 1 0 0 0 1 1h11a1 1 0 0 0 1-1v-2a5.006 5.006 0 0 0-5-5Z"/>
+                        </svg>
+                        <span class="flex-1 ml-3 whitespace-nowrap">Users</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="#" class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
+                        <svg class="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 18">
+                            <path d="M16 0H4a2 2 0 0 0-2 2v1H1a1 1 0 0 0 0 2h1v2H1a1 1 0 0 0 0 2h1v2H1a1 1 0 0 0 0 2h1v2H1a1 1 0 0 0 0 2h1v1a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2Zm-5.5 4.5a3 3 0 1 1 0 6 3 3 0 0 1 0-6ZM13.929 17H7.071a.5.5 0 0 1-.5-.5 3.935 3.935 0 1 1 7.858 0 .5.5 0 0 1-.5.5Z"/>
+                        </svg>
+                        <span class="flex-1 ml-3 whitespace-nowrap">Pemilih</span>
+                    </a>
+                </li>
+                <br><br><br><br><br><br><br><br><br><br><br>
+            </ul>
+
+            <ul class="pt-[10%] mt-[100%] space-y-2 font-medium border-t border-gray-200 dark:border-gray-700">
+                <li>
+                    <a href="#" class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
+                        <svg class="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 16">
+                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 8h11m0 0L8 4m4 4-4 4m4-11h3a2 2 0 0 1 2 2v10a2 2 0 0 1-2 2h-3"/>
+                        </svg>
+                        <span class="flex-1 ml-3 whitespace-nowrap">Logout</span>
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </aside>
+
+    <div class="p-4 sm:ml-64">
+        <div class="p-4 mt-1">
+            <div class="container mx-auto mt-[25px]">
+                <div>
+                    <nav class="justify-between py-3 text-gray-700 sm:flex" aria-label="Breadcrumb">
+                        <ol class="inline-flex items-center mb-3 space-x-1 md:space-x-3 sm:mb-0">
+                            <li>
+                                <div>
+                                    <svg class="w-[20px] h-[20px] text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                                        <path d="m19.707 9.293-2-2-7-7a1 1 0 0 0-1.414 0l-7 7-2 2a1 1 0 0 0 1.414 1.414L2 10.414V18a2 2 0 0 0 2 2h3a1 1 0 0 0 1-1v-4a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v4a1 1 0 0 0 1 1h3a2 2 0 0 0 2-2v-7.586l.293.293a1 1 0 0 0 1.414-1.414Z"/>
+                                    </svg>
+                                </div>
+                            </li>
+                            <li>
+                                <div class="flex items-center">
+                                    <a href="#" class="ml-1 text-sm font-medium text-gray-700 hover:text-blue-600 md:ml-2 dark:text-gray-400 dark:hover:text-white">@yield('judul1')</a>
+                                </div>
+                            </li>
+                            <li aria-current="page">
+                                <div class="flex items-center">
+                                    <svg class="w-3 h-3 mx-1 text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
+                                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 9 4-4-4-4"/>
+                                    </svg>
+                                    <a href="#" class="ml-1 text-sm font-medium text-gray-700 hover:text-blue-600 md:ml-2 dark:text-gray-400 dark:hover:text-white">@yield('judul2')</a>
+                                </div>
+                            </li>
+                        </ol>
+                    </nav>
+                </div>
+                @yield('body')
+            </div>
+        </div>
+    </div>
+    <script src="{{ asset('js/app.js') }}"></script>
+</body>
+</html>
