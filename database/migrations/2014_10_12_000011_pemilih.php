021412e1b146474f98b0f9e425034cb1de2542c9
Apriyono <apriyono992@gmail.com>
2023-07-24 14:56:30 +0700

    feat : mogration

--- database/migrations/2014_10_12_000011_pemilih.php
+++ database/migrations/2014_10_12_000011_pemilih.php
@@ -4,8 +4,13 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateTables extends Migration
+class Pemilih extends Migration
 {
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
     public function up()
     {
         Schema::create('pemilih', function (Blueprint $table) {
@@ -22,33 +27,17 @@ class CreateTables extends Migration
             $table->string('keterangan')->nullable();
             $table->string('status')->nullable();
             $table->integer('created_by')->nullable();
-            $table->foreign('created_by')->references('id')->on('users');
-            $table->timestamps();
-        });
-
-        Schema::create('role', function (Blueprint $table) {
-            $table->id();
-            $table->string('name')->nullable();
-        });
-
-        Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name')->nullable();
-            $table->string('email')->nullable();
-            $table->string('img_url')->nullable();
-            $table->string('password')->nullable();
-            $table->string('salt')->nullable();
-            $table->string('status')->nullable();
-            $table->integer('role_id')->nullable();
-            $table->foreign('role_id')->references('id')->on('role');
             $table->timestamps();
         });
     }
 
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
     public function down()
     {
         Schema::dropIfExists('pemilih');
-        Schema::dropIfExists('users');
-        Schema::dropIfExists('role');
     }
 }
