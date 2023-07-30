a2b961a6b8fc6264d06af9ce7e76b74db0cbe77f
fernandaiqshal111 <fernandaiqshal111@gmail.com>
2023-07-30 21:13:40 +0700

    fix: dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -125,45 +125,48 @@
                         </td>
                         <td class="px-6 py-4">
                             @if($value->status == 'pending')
-                            <button data-modal-target="popup-modal" data-modal-toggle="popup-modal" type="button">
+                            <button data-modal-target="popup-modal-{{$value->id}}" data-modal-toggle="popup-modal-{{$value->id}}" type="button">
                                 <img src="assets/dashboard/detail_pemilih.png" alt="detail_pemilih">
                             </button>
                             @endif
-
-                            <div id="popup-modal" tabindex="-1" class="fixed top-0 left-0 right-0 z-50 hidden p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-[calc(100%-1rem)] max-h-full">
-                                <div class="relative w-full max-w-md max-h-full">
-                                    <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
-                                        <button type="button" class="absolute top-3 right-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ml-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-hide="popup-modal">
-                                            <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
-                                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
-                                            </svg>
-                                            <span class="sr-only">Close modal</span>
-                                        </button>
-                                        <div class="p-10 text-center">
-                                            <div class="flex justify-between">
-                                                <a href="/dashboard-edit/{{$value->id}}/?status=rejected" class="w-full text-white bg-red-600 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm  items-center px-5 py-2.5 text-center mr-2">
-                                                    <button data-modal-hide="popup-modal" type="button">
-                                                        Reject
-                                                    </button>
-                                                </a>
-                                                <a href="/dashboard-edit/{{$value->id}}/?status=approved" class="w-full text-white bg-green-600 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 dark:focus:ring-green-800 font-medium rounded-lg text-sm items-center px-5 py-2.5 text-center mr-2">
-                                                    <button data-modal-hide="popup-modal" type="button">
-                                                            Approve
-                                                    </button>
-                                                </a>
-                                            </div>
-                                            <button data-modal-hide="popup-modal" type="button" class="w-full mt-[21px] text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-gray-200 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600">
-                                                cancel
-                                            </button>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
                         </td>
                     </tr>
                 @endforeach
             </tbody>
         </table>
+            @foreach($data as $value)
+                <div id="popup-modal-{{$value->id}}" tabindex="-1" class="fixed top-0 left-0 right-0 z-50 hidden p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-[calc(100%-1rem)] max-h-full">
+                    <div class="relative w-full max-w-md max-h-full">
+                        <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
+                            <button type="button" class="absolute top-3 right-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ml-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-hide="popup-modal-{{$value->id}}">
+                                <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
+                                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
+                                </svg>
+                                <span class="sr-only">Close modal</span>
+                            </button>
+                            <div class="p-10 text-center">
+                                <div class="flex justify-between">
+                                    <a href="/dashboard-edit/{{$value->id}}/?status=rejected" class="w-full text-white bg-red-600 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm  items-center px-5 py-2.5 text-center mr-2">
+                                        <button data-modal-hide="popup-modal" type="button">
+                                            Reject
+                                        </button>
+                                    </a>
+                                    <a href="/dashboard-edit/{{$value->id}}/?status=approved" class="w-full text-white bg-green-600 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 dark:focus:ring-green-800 font-medium rounded-lg text-sm items-center px-5 py-2.5 text-center mr-2">
+                                        <button data-modal-hide="popup-modal" type="button">
+                                            Approve
+                                        </button>
+                                    </a>
+                                </div>
+                                <button data-modal-hide="popup-modal" type="button" class="w-full mt-[21px] text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-gray-200 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600">
+                                    cancel
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+            @endforeach
+
     </div>
 
     {{ $data->appends(request()->except('page'))->onEachSide(1)->links('partials.pagination') }}
